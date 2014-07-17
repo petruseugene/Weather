32b08a8ec2d8766ccf82866b55b8da565946d4b8
epetrus <eugene.petrus@dataart.com>
2014-07-17 14:03:29 +0300

    Merge remote-tracking branch 'remotes/origin/master' into HEAD

--- src/com/example/weather/update/GetWeatherService.java
+++ src/com/example/weather/update/GetWeatherService.java
@@ -87,7 +87,7 @@ public class GetWeatherService extends Service {
 		WeatherWidget.updateWidget(this);
 		if(showNotifications) {
 			CityObject defCity = db.getDefaultCity();
-			Notification.notification(this, defCity, db.getWeatherObjects(defCity.getServerCityId())[0]);
+			Notification.notificate(this, defCity, db.getWeatherObjects(defCity.getServerCityId())[0]);
 		}
 	}
 	
