8529197594224f73d2acd982139f4f25882caf12
epetrus <eugene.petrus@dataart.com>
2014-07-22 11:37:47 +0300

    Some small changes.

--- src/com/example/weather/activities/WeatherLoadingActivity.java
+++ src/com/example/weather/activities/WeatherLoadingActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+
 import com.example.weather.R;
 import com.example.weather.update.AlarmManagerBroadcastReceiver;
 import com.example.weather.update.BootReceiver;
@@ -16,7 +17,7 @@ import com.example.weather.update.GetWeatherService;
 
 public class WeatherLoadingActivity extends Activity {
 	
-	public static final int SERVICE_ALARM_DELAY = 1000 * 3600;
+	public static final int SERVICE_ALARM_DELAY_MS = 3600000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +31,7 @@ public class WeatherLoadingActivity extends Activity {
 			AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
             Intent recieverIntent = new Intent(this, AlarmManagerBroadcastReceiver.class);
             PendingIntent pIntent = PendingIntent.getBroadcast(this, 0, recieverIntent, 0);
-            am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), SERVICE_ALARM_DELAY, pIntent);
+            am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), SERVICE_ALARM_DELAY_MS, pIntent);
 		}
 	}
 	
